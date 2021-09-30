f6f50be325a8ef828c6a63084cbf73aa026f941c
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2021-09-30 01:30:50 -0300

    Added a bunch of things

--- main.lua
+++ main.lua
@@ -6,6 +6,15 @@ local measure_label = Instance.new("TextLabel")
 local vertex_distance_label = Instance.new("TextLabel")
 local f_to_noclip = Instance.new("TextButton")
 local toggle_speed = Instance.new("TextButton")
+local clear_vertices = Instance.new("TextButton")
+local notes = Instance.new("TextBox")
+local add_labels_button = Instance.new("TextButton")
+local label_textbox = Instance.new("TextBox")
+local draw_tool = Instance.new("TextButton")
+local clear_drawings = Instance.new("TextButton")
+local credits = Instance.new("TextLabel")
+local credits2 = Instance.new("TextLabel")
+local credits3 = Instance.new("TextLabel")
 local open_close = Instance.new("Frame")
 local open_close_button = Instance.new("TextButton")
 
@@ -19,7 +28,7 @@ main.Parent = ScreenGui
 main.BackgroundColor3 = Color3.fromRGB(17, 0, 209)
 main.BackgroundTransparency = 0.200
 main.BorderSizePixel = 0
-main.Position = UDim2.new(0.385036469, 0, 0.199356914, 0)
+main.Position = UDim2.new(0.371853799, 0, 0.199356914, 0)
 main.Size = UDim2.new(0, 667, 0, 356)
 
 title.Name = "title"
@@ -75,9 +84,9 @@ vertex_distance_label.TextWrapped = true
 
 f_to_noclip.Name = "f_to_noclip"
 f_to_noclip.Parent = main
-f_to_noclip.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+f_to_noclip.BackgroundColor3 = Color3.fromRGB(52, 0, 125)
 f_to_noclip.BorderSizePixel = 0
-f_to_noclip.Position = UDim2.new(0.310344815, 0, 0.0196629222, 0)
+f_to_noclip.Position = UDim2.new(0.505247355, 0, 0.0196629167, 0)
 f_to_noclip.Size = UDim2.new(0, 139, 0, 31)
 f_to_noclip.Font = Enum.Font.SourceSans
 f_to_noclip.Text = "F to noclip"
@@ -88,9 +97,9 @@ f_to_noclip.TextWrapped = true
 
 toggle_speed.Name = "toggle_speed"
 toggle_speed.Parent = main
-toggle_speed.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+toggle_speed.BackgroundColor3 = Color3.fromRGB(52, 0, 125)
 toggle_speed.BorderSizePixel = 0
-toggle_speed.Position = UDim2.new(0.535232365, 0, 0.0196629222, 0)
+toggle_speed.Position = UDim2.new(0.730134904, 0, 0.0196629167, 0)
 toggle_speed.Size = UDim2.new(0, 121, 0, 31)
 toggle_speed.Font = Enum.Font.SourceSans
 toggle_speed.Text = "Toggle speed"
@@ -98,6 +107,126 @@ toggle_speed.TextColor3 = Color3.fromRGB(255, 149, 0)
 toggle_speed.TextScaled = true
 toggle_speed.TextSize = 42.000
 toggle_speed.TextWrapped = true
+
+clear_vertices.Name = "clear_vertices"
+clear_vertices.Parent = main
+clear_vertices.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+clear_vertices.BorderSizePixel = 0
+clear_vertices.Position = UDim2.new(0.310344785, 0, 0.0196629222, 0)
+clear_vertices.Size = UDim2.new(0, 121, 0, 31)
+clear_vertices.Font = Enum.Font.SourceSans
+clear_vertices.Text = "Clear vertices"
+clear_vertices.TextColor3 = Color3.fromRGB(255, 149, 0)
+clear_vertices.TextScaled = true
+clear_vertices.TextSize = 42.000
+clear_vertices.TextWrapped = true
+
+notes.Name = "notes"
+notes.Parent = main
+notes.BackgroundColor3 = Color3.fromRGB(3, 0, 26)
+notes.BorderSizePixel = 0
+notes.Position = UDim2.new(0, 0, 0.44101125, 0)
+notes.Size = UDim2.new(0, 363, 0, 199)
+notes.ClearTextOnFocus = false
+notes.Font = Enum.Font.SourceSans
+notes.MultiLine = true
+notes.Text = "Write your notes here!"
+notes.TextColor3 = Color3.fromRGB(255, 255, 255)
+notes.TextSize = 21.000
+notes.TextXAlignment = Enum.TextXAlignment.Left
+notes.TextYAlignment = Enum.TextYAlignment.Top
+
+add_labels_button.Name = "add_labels_button"
+add_labels_button.Parent = main
+add_labels_button.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+add_labels_button.BorderSizePixel = 0
+add_labels_button.Position = UDim2.new(0.310344815, 0, 0.126404494, 0)
+add_labels_button.Size = UDim2.new(0, 215, 0, 31)
+add_labels_button.Font = Enum.Font.SourceSans
+add_labels_button.Text = "Left alt + click to add labels"
+add_labels_button.TextColor3 = Color3.fromRGB(255, 149, 0)
+add_labels_button.TextScaled = true
+add_labels_button.TextSize = 42.000
+add_labels_button.TextWrapped = true
+
+label_textbox.Name = "label_textbox"
+label_textbox.Parent = main
+label_textbox.BackgroundColor3 = Color3.fromRGB(78, 78, 255)
+label_textbox.BorderSizePixel = 0
+label_textbox.Position = UDim2.new(0.644677639, 0, 0.126404494, 0)
+label_textbox.Size = UDim2.new(0, 178, 0, 31)
+label_textbox.ClearTextOnFocus = false
+label_textbox.Font = Enum.Font.SourceSans
+label_textbox.Text = ""
+label_textbox.TextColor3 = Color3.fromRGB(0, 0, 0)
+label_textbox.TextScaled = true
+label_textbox.TextSize = 14.000
+label_textbox.TextWrapped = true
+
+draw_tool.Name = "draw_tool"
+draw_tool.Parent = main
+draw_tool.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+draw_tool.BorderSizePixel = 0
+draw_tool.Position = UDim2.new(0.310344815, 0, 0.230337083, 0)
+draw_tool.Size = UDim2.new(0, 121, 0, 31)
+draw_tool.Font = Enum.Font.SourceSans
+draw_tool.Text = "Draw tool"
+draw_tool.TextColor3 = Color3.fromRGB(255, 149, 0)
+draw_tool.TextScaled = true
+draw_tool.TextSize = 42.000
+draw_tool.TextWrapped = true
+
+clear_drawings.Name = "clear_drawings"
+clear_drawings.Parent = main
+clear_drawings.BackgroundColor3 = Color3.fromRGB(0, 0, 125)
+clear_drawings.BorderSizePixel = 0
+clear_drawings.Position = UDim2.new(0.505247355, 0, 0.230337083, 0)
+clear_drawings.Size = UDim2.new(0, 139, 0, 31)
+clear_drawings.Font = Enum.Font.SourceSans
+clear_drawings.Text = "Clear drawings"
+clear_drawings.TextColor3 = Color3.fromRGB(255, 149, 0)
+clear_drawings.TextScaled = true
+clear_drawings.TextSize = 42.000
+clear_drawings.TextWrapped = true
+
+credits.Name = "credits"
+credits.Parent = main
+credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+credits.BackgroundTransparency = 1.000
+credits.Position = UDim2.new(0.544227898, 0, 0.44101125, 0)
+credits.Size = UDim2.new(0, 158, 0, 33)
+credits.Font = Enum.Font.SourceSans
+credits.Text = "By JUKjacker"
+credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+credits.TextScaled = true
+credits.TextSize = 14.000
+credits.TextWrapped = true
+
+credits2.Name = "credits2"
+credits2.Parent = main
+credits2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+credits2.BackgroundTransparency = 1.000
+credits2.Position = UDim2.new(0.544227898, 0, 0.533707857, 0)
+credits2.Size = UDim2.new(0, 216, 0, 20)
+credits2.Font = Enum.Font.SourceSans
+credits2.Text = "https://discord.gg/4jFcGAaq2T"
+credits2.TextColor3 = Color3.fromRGB(255, 255, 255)
+credits2.TextScaled = true
+credits2.TextSize = 14.000
+credits2.TextWrapped = true
+
+credits3.Name = "credits3"
+credits3.Parent = main
+credits3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+credits3.BackgroundTransparency = 1.000
+credits3.Position = UDim2.new(0.544227898, 0, 0.589887619, 0)
+credits3.Size = UDim2.new(0, 289, 0, 19)
+credits3.Font = Enum.Font.SourceSans
+credits3.Text = "https://mrwitzbold.github.io/jukjacker.github.io/"
+credits3.TextColor3 = Color3.fromRGB(255, 255, 255)
+credits3.TextScaled = true
+credits3.TextSize = 14.000
+credits3.TextWrapped = true
 
 open_close.Name = "open_close"
 open_close.Parent = ScreenGui
@@ -118,11 +247,9 @@ open_close_button.TextSize = 46.000
 
 -- Scripts:
 
-local function RQIOYJY_fake_script() -- measure_distance_button.LocalScript 
+local function SBZFU_fake_script() -- measure_distance_button.LocalScript 
 	local script = Instance.new('LocalScript', measure_distance_button)
 
-	
-	
 	function run()
 		if workspace:FindFirstChild("vertex1") == nil then
 			local stoppedatvertex = Instance.new("IntValue")
@@ -143,14 +270,14 @@ local function RQIOYJY_fake_script() -- measure_distance_button.LocalScript
 		local Mouse = Plr:GetMouse()
 	
 		Mouse.Button1Down:connect(function()
+			if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
+			if not Mouse.Target then return end
 			local sound = Instance.new("Sound")
 			sound.SoundId = "rbxassetid://7541642708"
 			sound.Parent = workspace
 			sound.Volume = 0.5
 			sound.Playing = true
 			sound.Name = "boowomp"
-			if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
-			if not Mouse.Target then return end
 			local position = Mouse.Hit.p
 			local vertex = Instance.new("Part")
 			vertex.Shape = Enum.PartType.Ball
@@ -160,22 +287,32 @@ local function RQIOYJY_fake_script() -- measure_distance_button.LocalScript
 			vertex.Position = position
 			vertex.Parent = workspace
 			vertex.Name = "vertex" .. tostring(workspace.stoppedatvertex.Value) .. "a"
+			local billboard = Instance.new("BillboardGui")
+			billboard.ExtentsOffset = Vector3.new(0, 5, 0)
+			billboard.Parent = vertex
+			billboard.Size = UDim2.new(0, 50, 0, 50) -- {0, 70},{0, 70}
+			local label = Instance.new("TextLabel")
+			label.BackgroundTransparency = 1
+			label.TextScaled = true
+			label.TextColor3 = Color3.fromRGB(0, 0, 149)
+			label.Parent = billboard
+			label.Size = UDim2.new(0, 50, 0, 50) -- {0, 70},{0, 70}
+			label.Text = tostring(workspace.stoppedatvertex.Value)
+			label.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+			label.TextStrokeTransparency = 0
 			if workspace.stoppedatvertex.Value % 2 == 0 then
 				workspace.vertex1.Value = "vertex" .. tostring(workspace.stoppedatvertex.Value) .. "a"
 			else
 				workspace.vertex2.Value = "vertex" .. tostring(workspace.stoppedatvertex.Value) .. "a"
-				if workspace.vertex1.Value ~= "" then
-					local vertex1a = workspace:FindFirstChild(workspace.vertex1.Value)
-					local vertex2a = workspace:FindFirstChild(workspace.vertex2.Value)
-					local distance = (vertex1a.Position-vertex2a.Position).Magnitude
-					script.Parent.Parent.vertex_distance_label.Text = "Distance between last two vertices:" .. tostring(distance)
-				end
 			end
 			workspace.stoppedatvertex.Value = workspace.stoppedatvertex.Value + 1
 			for i,v in pairs(workspace:GetChildren()) do
 				if v.Name:find("vertex", 1, #"vertex") and v.ClassName == "Part" then
 					if v.Name ~= workspace.vertex1.Value and v.Name ~= workspace.vertex2.Value then
-						v:Remove()
+						v.Color = Color3.fromRGB(117, 117, 117)
+						if v:FindFirstChild("Beam") == true then
+							v.Beam.Color = ColorSequence.new(Color3.fromRGB(117, 117, 117))
+						end
 					end
 				end
 			end
@@ -194,6 +331,12 @@ local function RQIOYJY_fake_script() -- measure_distance_button.LocalScript
 				beam.Width0 = 0.5
 				beam.Width1 = 0.5
 			end
+			if workspace.vertex1.Value ~= "" and workspace.vertex2.Value ~= "" then
+				local vertex1a = workspace:FindFirstChild(workspace.vertex1.Value)
+				local vertex2a = workspace:FindFirstChild(workspace.vertex2.Value)
+				local distance = (vertex1a.Position-vertex2a.Position).Magnitude
+				script.Parent.Parent.vertex_distance_label.Text = "Distance between last two vertices: " .. tostring(distance)
+			end
 			wait(1.5)
 			for i,v in pairs(workspace:GetChildren()) do
 				if v.Name == "boowomp" then
@@ -205,8 +348,8 @@ local function RQIOYJY_fake_script() -- measure_distance_button.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(run)
 end
-coroutine.wrap(RQIOYJY_fake_script)()
-local function OANSU_fake_script() -- f_to_noclip.LocalScript 
+coroutine.wrap(SBZFU_fake_script)()
+local function LWZNR_fake_script() -- f_to_noclip.LocalScript 
 	local script = Instance.new('LocalScript', f_to_noclip)
 
 	
@@ -232,8 +375,8 @@ local function OANSU_fake_script() -- f_to_noclip.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(run)
 end
-coroutine.wrap(OANSU_fake_script)()
-local function LTGLQ_fake_script() -- toggle_speed.LocalScript 
+coroutine.wrap(LWZNR_fake_script)()
+local function IIOYAB_fake_script() -- toggle_speed.LocalScript 
 	local script = Instance.new('LocalScript', toggle_speed)
 
 	function run()
@@ -246,8 +389,163 @@ local function LTGLQ_fake_script() -- toggle_speed.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(run)
 end
-coroutine.wrap(LTGLQ_fake_script)()
-local function WRQQEG_fake_script() -- open_close_button.LocalScript 
+coroutine.wrap(IIOYAB_fake_script)()
+local function DJJQHTZ_fake_script() -- clear_vertices.LocalScript 
+	local script = Instance.new('LocalScript', clear_vertices)
+
+	function run()
+		for i,v in pairs(workspace:GetChildren()) do
+			if v.Name:match("vertex") ~= nil and v.ClassName == "Part" then
+				v:Remove()
+			end
+		end
+		if workspace:FindFirstChild("stoppedatvertex") then
+			workspace.stoppedatvertex.Value = 1
+			workspace.vertex1.Value = ""
+			workspace.vertex2.Value = ""
+		end
+	end
+	
+	script.Parent.MouseButton1Click:Connect(run)
+end
+coroutine.wrap(DJJQHTZ_fake_script)()
+local function AMURB_fake_script() -- add_labels_button.LocalScript 
+	local script = Instance.new('LocalScript', add_labels_button)
+
+	function run()
+		local Plr = game:GetService("Players").LocalPlayer
+		local Mouse = Plr:GetMouse()
+	
+		Mouse.Button1Down:connect(function()
+			if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftAlt) then return end
+			if not Mouse.Target then return end
+			local sound = Instance.new("Sound")
+			sound.SoundId = "rbxassetid://7541642708"
+			sound.Parent = workspace
+			sound.Volume = 0.5
+			sound.Playing = true
+			sound.Name = "boowomp"
+			local position = Mouse.Hit.p
+			local vertex = Instance.new("Part")
+			vertex.Shape = Enum.PartType.Ball
+			vertex.Size = Vector3.new(0.5,0.5,0.5)
+			vertex.Color = Color3.fromRGB(106, 106, 106)
+			vertex.Anchored = true
+			vertex.Position = position
+			vertex.Parent = workspace
+			vertex.Name = "alabel"
+			local billboard = Instance.new("BillboardGui")
+			billboard.ExtentsOffset = Vector3.new(0, 5, 0)
+			billboard.Parent = vertex
+			billboard.Size = UDim2.new(0, 50, 0, 50) -- {0, 70},{0, 70}
+			local label = Instance.new("TextLabel")
+			label.BackgroundTransparency = 1
+			label.TextScaled = true
+			label.TextColor3 = Color3.fromRGB(255, 0, 251)
+			label.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+			label.TextStrokeTransparency = 0
+			label.Parent = billboard
+			label.Size = UDim2.new(0, 50, 0, 50) -- {0, 70},{0, 70}
+			label.Text = script.Parent.Parent.label_textbox.Text
+		end)
+	end
+	
+	script.Parent.MouseButton1Click:Connect(run)
+end
+coroutine.wrap(AMURB_fake_script)()
+local function KKLUPUL_fake_script() -- draw_tool.LocalScript 
+	local script = Instance.new('LocalScript', draw_tool)
+
+	function run()
+		local plyr = game.Players.LocalPlayer
+		local c = plyr.Character
+		local RunService = game:service'RunService'
+		local mouse = game.Players.LocalPlayer:GetMouse()
+		local draw2 = false
+		local colorA = 1
+		local lastPos
+	
+		local tool = Instance.new("HopperBin", plyr.Backpack)
+		tool.Name = "Draw"
+	
+	
+	
+		mouse = plyr:GetMouse()
+	
+	
+		function draw(obj) -- 
+			local lastPos = obj.CFrame.p
+			coroutine.wrap(function()
+				while wait() do
+					if draw2 then
+						while draw2 do
+							RunService.Stepped:wait()
+							objC = obj:Clone()
+							objC.Parent = c
+							objC.Anchored = true
+							local distance = (lastPos- obj.CFrame.p).magnitude
+							objC.Size = Vector3.new(.2,.2,distance)
+							objC.CFrame = CFrame.new(lastPos,obj.Position)*CFrame.new(0,0,-distance/2)
+							lastPos = obj.CFrame.p
+						end
+					else
+						break
+					end
+				end
+			end)()
+		end
+	
+		tool.Selected:connect(function(mouse)
+	
+			mouse.Button1Down:connect(function(mouse)
+				local part = Instance.new("Part", c)
+				part.Name = "location"
+				part.BottomSurface = 0
+				part.TopSurface = 0
+				part.BrickColor = BrickColor.Black()
+				part.FormFactor = "Custom"
+				part.Size = Vector3.new(0.2, 0.2, 0.2)
+				part.Anchored = true
+				part.Locked = true
+				coroutine.wrap(function()
+					while part ~= nil do
+						part.CFrame = CFrame.new(plyr:GetMouse().Hit.p.x,plyr:GetMouse().Hit.p.y,plyr:GetMouse().Hit.p.z)
+						RunService.Stepped:wait()
+					end
+				end)()         
+				draw(part)
+				draw2 = true
+			end)
+			mouse.Button1Up:connect(function(mouse)
+				game:service'Debris':AddItem(part, 0)
+				draw2 = false
+				pcall(function()
+					lastPos = nil
+				end)
+	
+			end)
+	
+		end)
+	end
+	
+	script.Parent.MouseButton1Click:Connect(run)
+end
+coroutine.wrap(KKLUPUL_fake_script)()
+local function WIRUHH_fake_script() -- clear_drawings.LocalScript 
+	local script = Instance.new('LocalScript', clear_drawings)
+
+	function run()
+		for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+			if v.Name == "location" then
+				v:Remove()
+			end
+		end
+	end
+	
+	script.Parent.MouseButton1Click:Connect(run)
+end
+coroutine.wrap(WIRUHH_fake_script)()
+local function HGVI_fake_script() -- open_close_button.LocalScript 
 	local script = Instance.new('LocalScript', open_close_button)
 
 	function run()
@@ -262,4 +560,4 @@ local function WRQQEG_fake_script() -- open_close_button.LocalScript
 	
 	script.Parent.MouseButton1Click:Connect(run)
 end
-coroutine.wrap(WRQQEG_fake_script)()
+coroutine.wrap(HGVI_fake_script)()
